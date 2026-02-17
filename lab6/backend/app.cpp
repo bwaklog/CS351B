99dde817a937ba83b687e6492565f1f1d98258fc
Aditya Hegde <burrawta@gmail.com>
2026-02-17 19:45:00 +0530

    chore: added lab6 files

---
+++ lab6/backend/app.cpp
@@ -0,0 +1,78 @@
+#include <arpa/inet.h>
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    hostname[255] = '\0';
+
+    // Create socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "ERROR: Failed to create socket" << std::endl;
+        return 1;
+    }
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    // Bind to port 8080
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        std::cerr << "ERROR: Failed to bind to port 8080" << std::endl;
+        return 1;
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        std::cerr << "ERROR: Failed to listen on port 8080" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Server listening on port 8080 (hostname: " << hostname << ")"
+              << std::endl;
+
+    // Accept connections in loop
+    while (true) {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_size = sizeof(client_addr);
+
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                               &client_addr_size);
+
+        if (client_fd < 0)
+            continue;
+
+        char buffer[1024] = {0};
+        read(client_fd, buffer, 1024);
+
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+
+        int client_port = ntohs(client_addr.sin_port);
+
+        std::cout << "connection from " << client_ip << ":" << client_port
+                  << std::endl;
+
+        // Simple HTTP response
+        std::string body = "Served by backend: " + std::string(hostname) + "\n";
+        std::string response = "HTTP/1.1 200 OK\r\n";
+        response += "Content-Type: text/plain\r\n";
+        response += "Content-Length: " + std::to_string(body.length()) + "\r\n";
+        response += "Connection: close\r\n\r\n";
+        response += body;
+
+        send(client_fd, response.c_str(), response.length(), 0);
+        close(client_fd);
+    }
+
+    return 0;
+}
